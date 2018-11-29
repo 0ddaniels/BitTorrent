1d8c7ab06709d0d1f30329ba9d44c6e876beb405
0ddaniels <42749711+0ddaniels@users.noreply.github.com>
2018-11-29 13:58:15 -0600

    Add files via upload

--- user.js
+++ user.js
@@ -3,6 +3,9 @@ function User(tracker, torrent){
 
   let myTracker = tracker;
   let myTorrent = torrent;
+  
+  let torrentFile = [];
+  let torrentTrackerFile = tracket();
   let state = "seeds" || "leech";
   let downloadSpeed = Math.random * 25;
   let uploadSpeed = Math.random * 10;
@@ -14,20 +17,31 @@ function startDownload(){
 }
 
 function updateTracker(){
+  Update(myTracker);
   
 
 }
 
 function checkProgress(){
+  let sum = 0;
+  for(let i = 0; i < Percent.length; i++){
+    sum = sum + percent[i];
+  }
+  return sum/percent.length;
 
 }
 
 function leech(){
+  uploadSpeed = 0;
+  downloadSpeed = Math.random;
 
 }
 
 function seed(){
-
+  uploadSpeed =  Math.random * 10;
+  downloadSpeed = Math.random * 25;
+  update(uploadSpeed);
+  update(downloadSpeed);
 }
 
 }
